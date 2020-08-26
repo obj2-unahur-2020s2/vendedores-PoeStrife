198f88e09138cf6b8636f80552ee0611dd66906c
PoeStrife <54420363+PoeStrife@users.noreply.github.com>
2020-08-26 18:38:20 -0300

    Revert "Faltan los test"
    This reverts commit f5222581434816d9c376ad2ac40e4ee28178b760.

--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
@@ -1,15 +1,12 @@
 package ar.edu.unahur.obj2.vendedores
 
-import java.util.function.Predicate.not
-import kotlin.String as String1
-
 class Certificacion(val esDeProducto: Boolean, val puntaje: Int)
 
 abstract class Vendedor {
   // Acá es obligatorio poner el tipo de la lista, porque como está vacía no lo puede inferir.
   // Además, a una MutableList se le pueden agregar elementos
   val certificaciones = mutableListOf<Certificacion>()
-  val nombre = toString()
+
   // Definimos el método abstracto.
   // Como no vamos a implementarlo acá, es necesario explicitar qué devuelve.
   abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean
@@ -31,9 +28,6 @@ abstract class Vendedor {
   fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto }
 
   fun puntajeCertificaciones() = certificaciones.sumBy { c -> c.puntaje }
-
-  open fun vendedorInfluyente():
-
 }
 
 // En los parámetros, es obligatorio poner el tipo
@@ -41,7 +35,6 @@ class VendedorFijo(val ciudadOrigen: Ciudad) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return ciudad == ciudadOrigen
   }
-  override fun vendedorInfluyente(): Unit = false
 }
 
 // A este tipo de List no se le pueden agregar elementos una vez definida
@@ -49,54 +42,10 @@ class Viajante(val provinciasHabilitadas: List<Provincia>) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return provinciasHabilitadas.contains(ciudad.provincia)
   }
-  override fun vendedorInfluyente() = provinciasHabilitadas.sumBy {p -> p.poblacion } > 10000000
-
 }
 
 class ComercioCorresponsal(val ciudades: List<Ciudad>) : Vendedor() {
   override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
     return ciudades.contains(ciudad)
   }
-
-  fun tamañoDeListaSinRepetidos(): Int {
-    val listaSinRep = mutableListOf<Ciudad>()
-    val listaOriginal = mutableListOf<Ciudad>()
-    listaOriginal == ciudades
-    val primerElemento = listaOriginal.first()
-
-
-
-    while (listaOriginal.isEmpty().not()){
-      if (listaSinRep.contains(primerElemento).not()){
-        listaSinRep.add(primerElemento)
-      }
-      listaOriginal.remove(primerElemento)
-    }
-    return listaOriginal.size
-    
-  }
-
-
-  override fun vendedorInfluyente(): Boolean {
-    return tamañoDeListaSinRepetidos() > 2 || ciudades.size > 4
-  }
 }
-abstract class centrosDeDistribucion(val ciudades: List<Ciudad>): Vendedor() {
-  val vendedores = mutableListOf<Vendedor>()
-  fun agregarVendedor(vendedor: Vendedor) {
-    if ((vendedores.any { v -> v.nombre == vendedor.toString() }).not()) {
-      vendedores.add(vendedor)
-    } else {
-      error("Ya esta el vendedor")
-    }
-
-    fun vendedorEstrella() = vendedores.map { v -> v.puntajeCertificaciones() }.max()
-
-    fun puedeCubrir(ciudad: Ciudad) = vendedores.filter { v -> v.puedeTrabajarEn(ciudad) }
-
-    fun vendedoresGenericos() = vendedores.filter { v -> v.certificacionesDeProducto() > 0 }
-
-    fun esRobusto() = vendedores.filter { v -> v.esFirme() }.size > 2
-  }
-}
-
